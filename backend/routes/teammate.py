87463cba8c9e188492628777bee77e3df2495764
Dichik <omeluan.dima@gmail.com>
2023-03-04 22:40:37 +0200

    fix: add /search_for_members

--- backend/routes/teammate.py
+++ backend/routes/teammate.py
@@ -4,12 +4,25 @@ from fastapi import APIRouter, Request
 
 from utils import compare_skills
 
-router = APIRouter(prefix="/api/team")
+router = APIRouter(prefix="/api")
 
 
 # FIXME (in the future) this method sucks
-@router.get('/')
+@router.get('/search_for_members')
 def find_team(skills: List[str], request: Request):
+    return find(skills, request)
+
+
+@router.get('/{project_id}/search_for_members')
+def find_team_by_project_id(project_id: str, request: Request):
+    project_query = {"_id": project_id}
+    project = request.app.database.projects.find_one(project_query)
+
+    skills = project['skills']
+    return find(skills, request)
+
+
+def find(skills, request: Request):
     result = []
     users = request.app.database.users.find_all()
     for user in users:

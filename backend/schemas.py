818424463ea46864f725a41f40d396bca55f3e0d
Andrii Smidonov <yukihirosm@gmail.com>
2023-03-04 14:40:30 +0200

    Add initial schemas structure

--- backend/schemas.py
+++ backend/schemas.py
@@ -1,8 +1,34 @@
 from typing import List, Union
-
+from datetime import date
 from pydantic import BaseModel
 
 
 class AuthItem(BaseModel):
     username: str
     password: str
+
+
+class ExperienceItem(BaseModel):
+    company: str
+    position: str
+    start: date
+    end: date
+    employment: str
+    place: str
+    description: str
+
+
+class EducationItem(BaseModel):
+    title: str
+    degree: str
+    place: str
+    start: str
+    end: str
+
+
+class ResumeItem(BaseModel):
+    about: str
+    skills: List[str]
+    experience: List[ExperienceItem]
+    education: List[EducationItem]
+    contact: str

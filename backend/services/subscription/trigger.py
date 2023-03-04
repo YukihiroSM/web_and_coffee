082cb837d3e82a8ec15d94183a1aeeda058977a7
Dichik <omeluan.dima@gmail.com>
2023-03-04 21:28:48 +0200

    create trigger schema to send emails about project

---
+++ backend/services/subscription/trigger.py
@@ -0,0 +1,8 @@
+from typing import List
+
+from schemas import ProjectItem
+
+
+def send_notifications_on_event(project: ProjectItem, emails: List[str]):
+    pass
+    # TODO send project info to each email in the emails list

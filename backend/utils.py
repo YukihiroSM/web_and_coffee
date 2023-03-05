ebac37bb946c99e65ad68314d5e23e91736df0df
Teasotea <sofi16616@gmail.com>
2023-03-05 02:48:00 +0200

    add email confirmation

--- backend/utils.py
+++ backend/utils.py
@@ -1,3 +1,5 @@
+from itsdangerous import URLSafeTimedSerializer
+
 def compare_skills(given, proposed):
     number = 0
     for skill in proposed:
@@ -6,4 +8,21 @@ def compare_skills(given, proposed):
 
 
 def compare_name(given, propose):
-    pass
+    # implement comparison with 2-3 letters wrong (Levenshtain dist or so)
+    return given==propose
+
+def generate_confirmation_token(email):
+    serializer = URLSafeTimedSerializer('SECRET_KEY')
+    return serializer.dumps(email, salt='SECURITY_PASSWORD_SALT')
+
+def confirm_token(token, expiration=3600):
+    serializer = URLSafeTimedSerializer('SECRET_KEY')
+    try:
+        email = serializer.loads(
+            token,
+            salt='SECURITY_PASSWORD_SALT',
+            max_age=expiration
+        )
+    except:
+        return False
+    return email
